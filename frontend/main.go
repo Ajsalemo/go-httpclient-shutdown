f126de22eefc1d7cd73ee0172b8632eb71d9eb17
Anthony Salemo <ajssalemo@gmail.com>
2025-06-14 15:37:27 -0400

    able to invoke request logic when a signal is recieved

--- frontend/main.go
+++ frontend/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"go-k8seenvsc-shutdown-frontend/config"
 	"go-k8seenvsc-shutdown-frontend/controllers"
@@ -14,6 +15,51 @@ import (
 
 func init() {
 	zap.ReplaceGlobals(zap.Must(zap.NewProduction()))
+}
+
+func makeRequestsOnShutdown() {
+	backendURL := config.GetBackendURL()
+	zap.L().Info("Making a request to backend", zap.String("backendURL", backendURL))
+	request := fiber.Get(backendURL)
+	_, data, err := request.Bytes()
+	if err != nil {
+		zap.L().Error("Error making request", zap.Any("error", err))
+	}
+
+	zap.L().Info("Request successful", zap.String("data", string(data)))
+}
+
+func initiateShutdownCountdown() {
+	ticker := time.NewTicker(2000 * time.Millisecond)
+	defer ticker.Stop()
+	start := time.Now()
+
+	shutdownTimeLimit := os.Getenv("SHUTDOWN_TIME_LIMIT")
+	if shutdownTimeLimit == "" {
+		shutdownTimeLimit = "15"
+	}
+	// Parse the shutdownTimeLimit (of type string) into a time.Duration
+	duration, err := time.ParseDuration(shutdownTimeLimit + "s")
+	if err != nil {
+		zap.L().Error("Error parsing SHUTDOWN_TIME_LIMIT", zap.Error(err))
+		return
+	}
+	// When a signal is caught, loop over this every 2 seconds up until whatever time SHUTDOWN_TIME_LIMIT  is set to
+	// in which at that point, this function should return and the app will actually exit
+	for {
+		select {
+		case <-ticker.C:
+			elapsed := time.Since(start)
+			// If the elapsed time is greater than 100 seconds, return a 500
+			if elapsed > duration {
+				zap.L().Info("Elapsed time: " + elapsed.String())
+				zap.L().Warn("SHUTDOWN_TIME_LIMIT was reached, exiting application..")
+				return
+			}
+
+			makeRequestsOnShutdown()
+		}
+	}
 }
 
 func main() {
@@ -32,15 +78,19 @@ func main() {
 		switch sig {
 		case os.Interrupt:
 			zap.L().Warn("CTRL+C / os.Interrupt recieved, shutting down the application..")
+			initiateShutdownCountdown()
 			app.Shutdown()
 		case syscall.SIGTERM:
 			zap.L().Warn("SIGTERM recieved.., shutting down the application..")
+			initiateShutdownCountdown()
 			app.Shutdown()
 		case syscall.SIGQUIT:
 			zap.L().Warn("SIGQUIT recieved.., shutting down the application..")
+			initiateShutdownCountdown()
 			app.Shutdown()
 		case syscall.SIGINT:
 			zap.L().Warn("SIGINT recieved.., shutting down the application..")
+			initiateShutdownCountdown()
 			app.Shutdown()
 		}
 	}()
